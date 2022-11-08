899286e9943d8c2a77accc6270b0fd8de0c3ba51
이고운 <kowoon@igoun-ui-MacBookAir.local>
2022-11-08 17:09:48 +0900

    Fix: 가독성 좋게 수정

--- src/index.js
+++ src/index.js
@@ -4,17 +4,23 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 //data를 modify하는 함수
 const countModifier = (count = 0, action) => {
-  console.log(count, action);
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
+
 //data를 저장하는 곳
 const countStore = createStore(countModifier);
 
@@ -22,18 +28,19 @@ const onChange = () => {
   number.innerText = countStore.getState();
 };
 
+//change를 store에서 감지하고 싶다면 그 change를 구독하면 됨.
 countStore.subscribe(onChange);
 
 //data의 store를 만들고(create하고)
 //data의 modifier가 countModifier이라는 것을 알고
 // 메세지를 그 store에 보내면 됨.
-//전송한 메세지는 action에 넣으면 되고 내가 할 일은 action을 체크해보면 됨.
+//전송한 메세지는 action에 넣으면 되고 내가 할 일은 action을 체크해보면 됨. (action은 무조건 객체)
 
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: ADD });
 };
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 };
 add.addEventListener("click", handleAdd);
 minus.addEventListener("click", handleMinus);
