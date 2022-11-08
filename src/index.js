69e0054d07212c1d69a90ff23fa40891c7ac2871
이고운 <kowoon@igoun-ui-MacBookAir.local>
2022-11-08 21:58:09 +0900

    Modify: delete 버튼 생성

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { toHaveDescription } from "@testing-library/jest-dom/dist/matchers";
 import { formDataToBlob } from "formdata-polyfill/esm.min";
 import { createStore } from "redux";
 
@@ -58,7 +59,8 @@ const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
       //return state.push(action.text) 이게 아니라 새로운 배열을 return 해야함.
-      return [...state, { text: action.text, id: Date.now() }];
+      //...state를 뒤로 보내서 array 가 보이는 방식을 수정할 수 있음.
+      return [{ text: action.text, id: Date.now() }, ...state];
     case DELETE_TODO:
       return [];
     default:
@@ -66,14 +68,45 @@ const reducer = (state = [], action) => {
   }
 };
 const store = createStore(reducer);
+
 store.subscribe(() => {
   console.log(store.getState());
 });
+
+const addToDo = (text) => {
+  store.dispatch({ type: ADD_TODO, text });
+};
+
+//parentNode를 알아야함. 왜냐면 삭제할 todo의 id가 필요하기 때문
+const deleteToDo = (e) => {
+  const id = e.target.parentNode.id;
+  store.dispatch({ type: DELETE_TODO, id });
+};
+
+const paintToDos = () => {
+  const toDos = store.getState();
+
+  //이렇게 하면 계속 누적해서 리스트 쌓임. 아래 입력해줘야함.
+  ul.innerHTML = "";
+
+  toDos.forEach((toDo) => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", deleteToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
+};
+
+store.subscribe(paintToDos);
 
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  store.dispatch({ type: ADD_TODO, text: toDo });
+  addToDo(toDo);
 };
 form.addEventListener("submit", onSubmit);
