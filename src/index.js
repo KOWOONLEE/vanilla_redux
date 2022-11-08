d815dfcd116854a975780421f14ae473422e6469
이고운 <kowoon@igoun-ui-MacBookAir.local>
2022-11-08 17:01:19 +0900

    Modify: redux이용하여 숫자 변경

--- src/index.js
+++ src/index.js
@@ -1,21 +1,39 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+//data를 modify하는 함수
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
+//data를 저장하는 곳
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+//data의 store를 만들고(create하고)
+//data의 modifier가 countModifier이라는 것을 알고
+// 메세지를 그 store에 보내면 됨.
+//전송한 메세지는 action에 넣으면 되고 내가 할 일은 action을 체크해보면 됨.
+
 const handleAdd = () => {
-  count = count + 1;
-  updateText();
+  countStore.dispatch({ type: "ADD" });
 };
 const handleMinus = () => {
-  count = count - 1;
-  updateText();
+  countStore.dispatch({ type: "MINUS" });
 };
 add.addEventListener("click", handleAdd);
 minus.addEventListener("click", handleMinus);
